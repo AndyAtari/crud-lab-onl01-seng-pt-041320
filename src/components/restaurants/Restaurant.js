227736db4fb83bf1204911569f551d61f0f9747d
Andrew Alteri <andrewsalteri@gmail.com>
2021-01-22 17:15:35 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,11 +1,12 @@
+
 import React, { Component } from 'react';
-import ReviewsContainer from '../../containers/ReviewsContainer';
+import ReviewsContainer from '../../containers/ReviewsContainer'
 
 class Restaurant extends Component {
 
-handleOnClick() {
-  this.props.deleteRestaurant(this.props.restaurant.id);
-}
+  handleOnClick() {
+    this.props.deleteRestaurant(this.props.restaurant.id);
+  }
 
   render() {
     const { restaurant } = this.props;
