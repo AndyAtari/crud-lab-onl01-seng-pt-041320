e3c467eb4bc92bb8865c7a197b42f81f2f0a8137
Andrew Alteri <andrewsalteri@gmail.com>
2021-01-22 16:58:53 +0000

    Automatically backed up by Learn

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  state = {
+    text: "",
+  }
+  
+  handleOnChange(event) {
+   this.setState({
+     text: event.target.value,
+   });
+ }
+
+ handleOnSubmit(event) {
+   event.preventDefault();
+   this.props.addRestaurant(this.state.text)
+   this.setState({
+     text: ''
+   });
+ }
+ 
   render() {
     return (
       <div>
-        Restaurant Input
+      
       </div>
     );
   }
